c6c1e0b4a9dd1ddf1533243c8522508e9dd4c6aa
Coral Fussman <coralfussman@gmail.com>
2020-03-18 19:16:38 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -51,8 +51,8 @@ def matrix_lookup(matrix, row, column)
      ["Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant"],
      ["Strawberries", "Potatoes", "Grapes", "Avocadoes","Asparagus"]
   ]
-  
-  storage_room[1][1]
+
+  storage_room[1][3]
 end
 
 def matrix_update(matrix, row, column, new_value)
